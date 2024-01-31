cf356bd16ac38ae4ced45173a84af655a17b511b
m23moren <michael.moreno@imt-atlantique.net>
2024-01-31 14:59:35 +0100

    Plusieurs mini projets

---
+++ Annuaire/gestion.js
@@ -0,0 +1,56 @@
+function ajouter(event){
+    event.preventDefault();
+    let table = document.getElementById("annuaire");
+    let nom=document.getElementById("Nom").value;
+    let prenom=document.getElementById("Prenom").value;
+    let tel=document.getElementById("Telephone").value;
+    let mail=document.getElementById("Mail").value;
+    let row = table.insertRow(1);
+
+    let cellNom = row.insertCell(0);
+    let cellPrenom = row.insertCell(1);
+    let cellTel = row.insertCell(2);
+    let cellMail = row.insertCell(3);
+    let cellMod = row.insertCell(4);
+    let cellSuppr = row.insertCell(5);
+    
+    cellNom.innerHTML = nom;
+    cellPrenom.innerHTML = prenom;
+    cellTel.innerHTML = tel;
+    cellMail.innerHTML = mail;
+
+    cellMod.classList.add("mod");
+
+    let buttonMod = document.createElement("button");
+    buttonMod.innerHTML = "Modifier";
+    buttonMod.addEventListener("click", function(){
+        console.log("holi");
+    });
+
+    cellSuppr.classList.add("elim");
+
+    let buttonSuppr = document.createElement("button");
+    buttonSuppr.innerHTML = "Supprimer";
+    buttonSuppr.addEventListener("click", function(){
+        supprimerLigne(this);
+    });
+
+    cellMod.appendChild(buttonMod);
+    cellSuppr.appendChild(buttonSuppr);
+}
+
+function supprimerLigne(button){
+    var row = button.parentNode.parentNode;
+
+    var table = document.getElementById("annuaire");
+
+    var rowIndex = row.rowIndex;
+    table.deleteRow(rowIndex);
+}
+
+document.getElementById('adder').addEventListener('click', function() {
+    //Montrer ou cacher le formulaire pour envoyer les donnes
+    var form = document.querySelector('form');
+    form.style.display = (form.style.display === 'none' || form.style.display === '') ? 'flex' : 'none';
+});
+
---
+++ Factoriel/factoriel.js
@@ -0,0 +1,17 @@
+
+function factoriel(event) {
+    event.preventDefault();
+    let introduced = document.getElementById("numerito").value;
+
+    if(introduced >0){
+        let factors = introduced-1;
+        while (factors > 0){
+            introduced *= factors;
+            factors-=1;
+        }
+        document.getElementById("result").innerHTML=introduced;
+    }
+    else{
+        document.getElementById("result").innerHTML="Nombre doit etre positive et plus que 0";
+    }
+}
